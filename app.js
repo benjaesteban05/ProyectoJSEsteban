3d7c1111d0d142f837660adc974a2fabcd239fb9
Benjamin <benjaesteban05@hotmail.com>
2023-10-18 20:18:06 -0300

    PreEntrega2Esteban

--- app.js
+++ app.js
@@ -1,78 +1,111 @@
+const shopContent = document.getElementById("shopContent")
+const verCarrito = document.getElementById("verCarrito")
+const modalContainer = document.getElementById("modal-container")
+
 const productos = [
-    {nombre: "Harina", precio: 280 },
-    {nombre: "Galletitas", precio: 230 },
-    {nombre: "Leche", precio: 590 },
-    {nombre: "Huevos", precio: 400 },
-    {nombre: "Azucar", precio: 270 },
-    {nombre: "Gaseosa", precio: 550 },
-    {nombre: "Jabon", precio: 160 },
-    {nombre: "Fideos", precio: 280 },
+    {
+        id: 1,
+        nombre: "Harina",
+        precio: 460,
+        img:"https://acdn.mitiendanube.com/stores/001/267/442/products/7792180139313_02-photoroom1-c91ec989b6b8ea789216894436656599-1024-1024.webp"
+    },
+    {
+        id: 2,
+        nombre: "Arroz",
+        precio: 350,
+        img:"https://media.f2h.shop/media/catalog/product/cache/ab45d104292f1bb63d093e6be8310c97/2/0/2020-08-14_r01_molinos_ala_doble_carolina_frente-xzhh_akzn-transformed.png"
+    },
+    {
+        id: 3,
+        nombre: "Aceite de oliva",
+        precio: 890,
+        img:"https://granjaselpato.com.ar/wp-content/uploads/2021/06/aceite-oliva-finca-sagrada-500ml-1.jpg"
+    },
+    {
+        id: 4,
+        nombre: "Galletitas",
+        precio: 750,
+        img:"https://elviejoparisvirtual.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBOU5CQ0E9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--64c759bfc4707b59e889bec156b11bb4eed2fa9d/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCem9MWm05eWJXRjBPZ2wzWldKd09oUnlaWE5wZW1WZmRHOWZiR2x0YVhSYkIya0NXQUpwQWxnQyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--821afe8460fe07e94582d7e96e8707ec016a9675/galleta-oreo-original-fca80eea-3592-4a9d-abb6-ecac20283ce7.jpg?locale=es"
+    },
+    {
+        id: 5,
+        nombre: "Leche",
+        precio: 1200,
+        img:"https://acdn.mitiendanube.com/stores/093/780/products/serenisima-clasica-751-95fea92d1a27f8e9ab15710914346750-1024-1024.webp"
+    },
+    {
+        id: 6,
+        nombre: "Pan lactal",
+        precio: 600,
+        img:"https://www.superacasa.com.ar/3095-large_default/pan-lactal-pan-de-mesa-rf-1p-500g-bolsa-lac.jpg"
+    }
 ];
 
-let carrito = [ ]
-let edad = prompt ("Bienvenido, por favor ingrese su edad")
-if (edad >= 18){
-    alert("Bienvenido")
-} else{
-    alert("Debe ser mayor de edad para ingresar")
-}
+let carrito = [];
 
-let seleccion = prompt ("Desea comprar algo?")
+productos.forEach((products)=> {
+    let content = document.createElement("div")
+    content.className = "productos"
+    content.innerHTML = `
+    <img src="${products.img}">
+    <h3>${products.nombre}</h3>
+    <p class="precio">${products.precio} $</p>
+    `;
 
-while(seleccion != "si" && seleccion !="no"){
-    alert("Por favor ingrese si o no")
-    seleccion = prompt("Desea comprar algo?")
-}
-if (seleccion == "si"){
-    alert("A continuacion ingrese lo que desea comprar.")
-}
-else if (seleccion == "no"){
-    alert("Gracias por venir, hasta pronto!")
-}
+    shopContent.append(content)
+    let comprar = document.createElement("button");
+    comprar.innerText = "comprar"
+    comprar.className = "comprar"
+    
+    content.append(comprar);
 
-while(seleccion != "no"){
-    let productos = prompt("Agregue un producto a su carrito")
-    let precio = 0
-
-    if(productos == "Harina" || productos == "Galletitas" || productos == "Leche" || productos == "Huevos" || productos == "Azucar" || productos == "Gaseosa" || productos == "Jabon" || productos == "Fideos"){ 
-        switch(productos){
-            case "Harina":
-            precio = 280
-            break;
-            case "Galletitas":
-            precio = 230
-            break;
-            case "Leche":
-            precio = 590
-            break;
-            case "Huevos":
-            precio = 400
-            break;
-            case "Azucar":
-            precio = 270
-            break;
-            case "Gaseosa":
-            precio = 550
-            break;
-            case "Jabon":
-            precio = 160
-            break;
-            case "Fideos":
-            precio = 280
-            break;
-            default:
-            break;
-        }
-        let unidades = parseInt(prompt("Cuantas unidades desea llevar?"))
-        carrito.push({productos, unidades, precio})
+    comprar.addEventListener("click" , () =>{
+        carrito.push({
+            id : products.id,
+            img : products.img,
+            nombre : products.nombre,
+            precio : products.precio,
+        })
         console.log(carrito)
-    } else{
-        alert ("No tenemos ese producto")
-    }
-    seleccion = prompt("Desea seguir comprando?")
-    while(seleccion == "no"){
-        alert("Gracias por su compra, hasta pronto!")
-        break;
-    }
-}
+    })
+});
 
+verCarrito.addEventListener("click" , () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex"
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+    <h1 class="modal-header-tittle">Carrito</h1>
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalButton = document.createElement("h2");
+    modalButton.innerText = "X";
+    modalButton.className = "modal-header-button";
+
+    modalButton.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    });
+
+    modalHeader.append(modalButton);
+
+    carrito.forEach((products) => {
+        let carritoContent = document.createElement("div")
+        carritoContent.className = "modal-content"
+        carritoContent.innerHTML = `
+            <img src="${products.img}">
+            <h3>${products.nombre}</h3>
+            <p>${products.precio} $</p>
+        `;
+
+        modalContainer.append(carritoContent)
+    })
+
+const total = carrito.reduce((acc, el) => acc+ el.precio, 0);
+
+const totalCompra = document.createElement("div")
+totalCompra.className = "total-content"
+totalCompra.innerHTML = `Total a pagar: ${total} $`;
+modalContainer. append(totalCompra)
+});
