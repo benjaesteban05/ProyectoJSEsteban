444bc802db4936c75a1c06a642eb85a0801ba49d
Benjamin <benjaesteban05@hotmail.com>
2023-11-06 14:06:44 -0300

    PreEntrega3Esteban

--- app.js
+++ app.js
@@ -1,47 +1,54 @@
 const shopContent = document.getElementById("shopContent")
 const verCarrito = document.getElementById("verCarrito")
 const modalContainer = document.getElementById("modal-container")
+const cantidadCarrito = document.getElementById("cantidadCarrito")
 
 const productos = [
     {
         id: 1,
         nombre: "Harina",
         precio: 460,
-        img:"https://acdn.mitiendanube.com/stores/001/267/442/products/7792180139313_02-photoroom1-c91ec989b6b8ea789216894436656599-1024-1024.webp"
+        img:"https://acdn.mitiendanube.com/stores/001/267/442/products/7792180139313_02-photoroom1-c91ec989b6b8ea789216894436656599-1024-1024.webp",
+        cantidad: 1,
     },
     {
         id: 2,
         nombre: "Arroz",
         precio: 350,
-        img:"https://media.f2h.shop/media/catalog/product/cache/ab45d104292f1bb63d093e6be8310c97/2/0/2020-08-14_r01_molinos_ala_doble_carolina_frente-xzhh_akzn-transformed.png"
+        img:"https://media.f2h.shop/media/catalog/product/cache/ab45d104292f1bb63d093e6be8310c97/2/0/2020-08-14_r01_molinos_ala_doble_carolina_frente-xzhh_akzn-transformed.png",
+        cantidad: 1,
     },
     {
         id: 3,
         nombre: "Aceite de oliva",
         precio: 890,
-        img:"https://granjaselpato.com.ar/wp-content/uploads/2021/06/aceite-oliva-finca-sagrada-500ml-1.jpg"
+        img:"https://granjaselpato.com.ar/wp-content/uploads/2021/06/aceite-oliva-finca-sagrada-500ml-1.jpg",
+        cantidad: 1,
     },
     {
         id: 4,
         nombre: "Galletitas",
         precio: 750,
-        img:"https://elviejoparisvirtual.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBOU5CQ0E9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--64c759bfc4707b59e889bec156b11bb4eed2fa9d/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCem9MWm05eWJXRjBPZ2wzWldKd09oUnlaWE5wZW1WZmRHOWZiR2x0YVhSYkIya0NXQUpwQWxnQyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--821afe8460fe07e94582d7e96e8707ec016a9675/galleta-oreo-original-fca80eea-3592-4a9d-abb6-ecac20283ce7.jpg?locale=es"
+        img:"https://elviejoparisvirtual.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBOU5CQ0E9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--64c759bfc4707b59e889bec156b11bb4eed2fa9d/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCem9MWm05eWJXRjBPZ2wzWldKd09oUnlaWE5wZW1WZmRHOWZiR2x0YVhSYkIya0NXQUpwQWxnQyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--821afe8460fe07e94582d7e96e8707ec016a9675/galleta-oreo-original-fca80eea-3592-4a9d-abb6-ecac20283ce7.jpg?locale=es",
+        cantidad: 1,
     },
     {
         id: 5,
         nombre: "Leche",
         precio: 1200,
-        img:"https://acdn.mitiendanube.com/stores/093/780/products/serenisima-clasica-751-95fea92d1a27f8e9ab15710914346750-1024-1024.webp"
+        img:"https://acdn.mitiendanube.com/stores/093/780/products/serenisima-clasica-751-95fea92d1a27f8e9ab15710914346750-1024-1024.webp",
+        cantidad: 1,
     },
     {
         id: 6,
         nombre: "Pan lactal",
         precio: 600,
-        img:"https://www.superacasa.com.ar/3095-large_default/pan-lactal-pan-de-mesa-rf-1p-500g-bolsa-lac.jpg"
+        img:"https://www.superacasa.com.ar/3095-large_default/pan-lactal-pan-de-mesa-rf-1p-500g-bolsa-lac.jpg",
+        cantidad: 1,
     }
 ];
 
-let carrito = [];
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 productos.forEach((products)=> {
     let content = document.createElement("div")
@@ -60,17 +67,31 @@ productos.forEach((products)=> {
     content.append(comprar);
 
     comprar.addEventListener("click" , () =>{
-        carrito.push({
+    const repeat = carrito.some((repeatProducts) => repeatProducts.id === products.id) 
+    
+    if(repeat){
+        carrito.map((prod) => {
+            if (prod.id == products.id) {
+                prod.cantidad++;
+            }
+        });
+    } else{
+    carrito.push({
             id : products.id,
             img : products.img,
             nombre : products.nombre,
             precio : products.precio,
-        })
+            cantidad: products.cantidad,
+        });
         console.log(carrito)
-    })
+        console.log(carrito.length)
+        carritoCounter()
+        saveLocal();
+    }
+    });
 });
 
-verCarrito.addEventListener("click" , () => {
+const pintarCarrito = () => {
     modalContainer.innerHTML = "";
     modalContainer.style.display = "flex"
     const modalHeader = document.createElement("div");
@@ -97,15 +118,79 @@ verCarrito.addEventListener("click" , () => {
             <img src="${products.img}">
             <h3>${products.nombre}</h3>
             <p>${products.precio} $</p>
+            <span class="restar"> - </span>
+            <p>Cantidad: ${products.cantidad}</p>
+            <span class="sumar"> + </span>
+            <P>Total: ${products.cantidad * products.precio}</P>
+            <span class="delete-product"> ❌ </span>
         `;
 
-        modalContainer.append(carritoContent)
-    })
+        modalContainer.append(carritoContent);
 
-const total = carrito.reduce((acc, el) => acc+ el.precio, 0);
+        let restar = carritoContent.querySelector(".restar")
+        
+        restar.addEventListener("click", () => {
+            if(products.cantidad !== 1){
+            products.cantidad--;
+        }
+            saveLocal()
+            pintarCarrito()
+        });
+
+        let sumar = carritoContent.querySelector(".sumar")
+        sumar.addEventListener("click", () => {
+            products.cantidad++;
+            saveLocal()
+            pintarCarrito()
+        });
+
+        let eliminar = carritoContent.querySelector(".delete-product");
+
+        eliminar.addEventListener("click", () => {
+            eliminarProducto(products.id);
+        })
+    // let eliminar = document.createElement("span");
+    // eliminar.innerText = "❌";
+    // eliminar.className = "delete-product";
+    // carritoContent.append(eliminar);
+
+    // eliminar.addEventListener("click", eliminarProducto);
+    });
+
+const total = carrito.reduce((acc, el) => acc+ el.precio * el.cantidad, 0);
 
 const totalCompra = document.createElement("div")
 totalCompra.className = "total-content"
 totalCompra.innerHTML = `Total a pagar: ${total} $`;
 modalContainer. append(totalCompra)
-});
+};
+
+verCarrito.addEventListener("click", pintarCarrito); 
+
+const eliminarProducto = (id) => {
+    const foundId = carrito.find((element) => element.id === id);
+
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== foundId;
+    });
+    carritoCounter();
+    saveLocal();
+    pintarCarrito();
+};
+
+const carritoCounter = ()=> {
+    cantidadCarrito.style.display = "block";
+
+const carritoLength = carrito.length;
+
+localStorage.setItem("carritoLength", JSON.stringify(carritoLength))
+
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
+};
+
+carritoCounter();
+
+const saveLocal = () => {
+    localStorage.setItem("carrito",JSON.stringify (carrito));
+}
+
