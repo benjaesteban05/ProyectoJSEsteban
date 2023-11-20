1c45321a19fe59b6f2b2c25e7b3156ceeb1f0951
Benjamin <benjaesteban05@hotmail.com>
2023-11-20 18:20:30 -0300

    proyectofinal-Esteban

--- app.js
+++ app.js
@@ -3,95 +3,6 @@ const verCarrito = document.getElementById("verCarrito")
 const modalContainer = document.getElementById("modal-container")
 const cantidadCarrito = document.getElementById("cantidadCarrito")
 
-<<<<<<< HEAD
-const productos = [
-    {
-        id: 1,
-        nombre: "Harina",
-        precio: 460,
-        img:"https://acdn.mitiendanube.com/stores/001/267/442/products/7792180139313_02-photoroom1-c91ec989b6b8ea789216894436656599-1024-1024.webp",
-        cantidad: 1,
-    },
-    {
-        id: 2,
-        nombre: "Arroz",
-        precio: 350,
-        img:"https://media.f2h.shop/media/catalog/product/cache/ab45d104292f1bb63d093e6be8310c97/2/0/2020-08-14_r01_molinos_ala_doble_carolina_frente-xzhh_akzn-transformed.png",
-        cantidad: 1,
-    },
-    {
-        id: 3,
-        nombre: "Aceite de oliva",
-        precio: 890,
-        img:"https://granjaselpato.com.ar/wp-content/uploads/2021/06/aceite-oliva-finca-sagrada-500ml-1.jpg",
-        cantidad: 1,
-    },
-    {
-        id: 4,
-        nombre: "Galletitas",
-        precio: 750,
-        img:"https://elviejoparisvirtual.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBOU5CQ0E9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--64c759bfc4707b59e889bec156b11bb4eed2fa9d/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCem9MWm05eWJXRjBPZ2wzWldKd09oUnlaWE5wZW1WZmRHOWZiR2x0YVhSYkIya0NXQUpwQWxnQyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--821afe8460fe07e94582d7e96e8707ec016a9675/galleta-oreo-original-fca80eea-3592-4a9d-abb6-ecac20283ce7.jpg?locale=es",
-        cantidad: 1,
-    },
-    {
-        id: 5,
-        nombre: "Leche",
-        precio: 1200,
-        img:"https://acdn.mitiendanube.com/stores/093/780/products/serenisima-clasica-751-95fea92d1a27f8e9ab15710914346750-1024-1024.webp",
-        cantidad: 1,
-    },
-    {
-        id: 6,
-        nombre: "Pan lactal",
-        precio: 600,
-        img:"https://www.superacasa.com.ar/3095-large_default/pan-lactal-pan-de-mesa-rf-1p-500g-bolsa-lac.jpg",
-        cantidad: 1,
-    }
-];
-
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-
-productos.forEach((products)=> {
-    let content = document.createElement("div")
-    content.className = "productos"
-    content.innerHTML = `
-    <img src="${products.img}">
-    <h3>${products.nombre}</h3>
-    <p class="precio">${products.precio} $</p>
-    `;
-
-    shopContent.append(content)
-    let comprar = document.createElement("button");
-    comprar.innerText = "comprar"
-    comprar.className = "comprar"
-    
-    content.append(comprar);
-
-    comprar.addEventListener("click" , () =>{
-    const repeat = carrito.some((repeatProducts) => repeatProducts.id === products.id) 
-    
-    if(repeat){
-        carrito.map((prod) => {
-            if (prod.id == products.id) {
-                prod.cantidad++;
-            }
-        });
-    } else{
-    carrito.push({
-            id : products.id,
-            img : products.img,
-            nombre : products.nombre,
-            precio : products.precio,
-            cantidad: products.cantidad,
-        });
-        console.log(carrito)
-        console.log(carrito.length)
-        carritoCounter()
-        saveLocal();
-    }
-    });
-});
-=======
 const productos = [];
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
@@ -147,7 +58,6 @@ const getProducts = async () => {
 
 getProducts();
 
->>>>>>> 3c74f3d (ProyectoFinalEsteban)
 
 const pintarCarrito = () => {
     modalContainer.innerHTML = "";
